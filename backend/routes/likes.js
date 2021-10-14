9e8d2ae9cd4b3328d3ffdd5c6e5b028c01388344
jcovington16 <covingtonjoshua7@gmail.com>
2021-10-13 18:38:57 -0600

    working on the likes functionality endpoint. needed to change likes to comment.

--- backend/routes/likes.js
+++ backend/routes/likes.js
@@ -2,19 +2,19 @@ const {Comment, validateComment} = require('../models/comment');
 const express = require('express');
 const router = express.Router();
 
-router.put('/:commentID', async(req, res) => {
+router.put('/likes/:commentID', async(req, res) => {
     try {
         const {error} = validateComment(req.body);
         if(error) {
             return res.status(400).send(error);
         }
 
-        const like = await Comment.findByIdAndUpdate(req.params.commentID, {
+        const comment = await Comment.findByIdAndUpdate(req.params.commentID, {
             likes: req.body.likes += 1
         })
         
-        await like.save();
-        return res.send(like)
+        await comment.save();
+        return res.send(comment)
 
     } catch(ex) {
         return res.status(500).send(`Internal Server Error: ${ex}`);
