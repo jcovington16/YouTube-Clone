c4823c6f02db7918727dcd68e453162db8b5b9dc
jcovington16 <covingtonjoshua7@gmail.com>
2021-10-19 17:00:54 -0600

    Working on displaying the videos to the main screen. The youtbue API is working

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Header.css';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import axios from 'axios';
 
 
 const Header = () => {
+
+    const url = `https://www.googleapis.com/youtube/v3/search?q=&key=`
+    const [videoSearch, setVideoSearch] = useState("");
+    const [videoID, setVideosID] = useState("");
+
+    useEffect(() => {
+        axios.get(url)
+            .then(response => setVideoSearch(response.data)
+        )}, [videoSearch])
     return (
         <div className="header">
 
@@ -22,7 +32,7 @@ const Header = () => {
 
             <div className="header__input">
                 <input type="text" placeholder="Search"/>
-                <SearchIcon className="header__inputButton"/>
+                <SearchIcon className="header__inputButton" type="submit"/>
             </div>
 
             <div className="header__right">
--- frontend/src/components/Videos.jsx
+++ frontend/src/components/Videos.jsx
@@ -6,20 +6,17 @@ const Videos = () => {
 
     const [videos, setVideos] = useState([]);
 
-    const searchTerms = ["factory%20functions","data%20structures", "array%20functions%20javascript", "composition%20over%20inheritance", "lambda%20functions", "streams%20java", "higher%20order%functions%javascript", "functional%20programming", "c++%20lambda%20functions", "sorting%20algorithms"];
+//    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=computers&key=${"apiKey"}`;
 
-    const getSearchTerm = () => searchTerms[Math.floor(Math.random() * (searchTerms.length-1))];
-
-    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${getSearchTerm()}&key=${"apiKey"}`;
 
     useEffect(() => {
-        axios.get(url)
-            .then(response => setVideos(response.data))
-    })
+        axios.get(`https://www.googleapis.com/youtube/v3/search?q= &key=`)
+            .then(response => setVideos(response.data["items"])
+        )}, [videos]);
 
     return (
         <div className="videos">
- 
+            <h2>Recommended</h2>
         </div>
     )
 }
