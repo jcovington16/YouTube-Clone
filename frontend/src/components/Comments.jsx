8ee5f2e2c3ad4943c50440a464ed88b869ece15a
RoamingKilroy <kilroy.zachary@gmail.com>
2021-10-24 14:26:32 -0400

    Update Comments.jsx

--- frontend/src/components/Comments.jsx
+++ frontend/src/components/Comments.jsx
@@ -4,16 +4,12 @@ import Replies from './Reply';
 
 const Comments = (props) => {
 
-   const url = "http://localhost:5001/api/comment/";
-   const [comments, setComments] = useState([]);
+   const [comment, setComments] = useState([]);
    const [text, setText] = useState('');
-   const [comment_id, setComment_id] = useState('');
 
     useEffect(()=> {
             axios.get(`http://localhost:5001/api/comment/${props.id}`)
-            .then(res => {setComment_id(res.data["_id"])})
-            axios.get(`http://localhost:5001/api/comment/${props.id}`)
-            .then(res=> {setComments(res.data["text"])})
+            .then(res=> {setComments(res.data)})
         })    
 
     const handleChange = (event) => {
@@ -28,23 +24,37 @@ const Comments = (props) => {
         }
     
         addNewComment(newComment);
-        setText('');
+        setText("");
     }
 
     const addNewComment = (newComment) => {
         axios.post(`http://localhost:5001/api/comment/`, newComment)   
       }
-
+      console.log(comment);
     return (
         <div>
             <h2>Comments</h2>
-            <ul><li>{comments}</li></ul>
+            <ul>            
+                {comment.map((a) => {
+                    return(
+                        <li key={a._id}>
+                            <p>{a.text}</p>
+
+                            <Replies id={a._id} />
+                        </li>
+                             
+                    )
+                    
+                })}
+            </ul>
+
+
             <form action="">
                <textarea style={{width: '100%', borderRadius: '5px'}}name="" placeholder="write some comments"onChange={(event)=>handleChange(event)}></textarea>
                <div className="replyButton">
                 <button onClick={handleClick}>Submit comment</button>
                 </div>
-                <Replies id={comment_id} />
+                
             </form>  
 
         </div>
